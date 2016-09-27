31430624bf6dea77f694b3a67419e7fe88cfa593
Shota Kaneko <s-kaneko@ostechnology.co.jp>
2016-09-27 15:57:30 +0900

    Initial Commit

---
+++ PullRequestPracticeTests/PullRequestPracticeTests.swift
@@ -0,0 +1,36 @@
+//
+//  PullRequestPracticeTests.swift
+//  PullRequestPracticeTests
+//
+//  Created by Shota Kaneko on 2016/09/27.
+//  Copyright © 2016年 kaneko. All rights reserved.
+//
+
+import XCTest
+@testable import PullRequestPractice
+
+class PullRequestPracticeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
